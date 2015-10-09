767d32ff0f55b4ac590522c2f8063378a0136b91
bvv27 <bvv.developer@gmail.com>
2015-10-09 15:27:13 +0300

    1

---
+++ TravisTest/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TravisTest
+{
+    class Class1
+    {
+        public bool Get(bool t)
+        {
+            return !t;
+        }
+    }
+}
