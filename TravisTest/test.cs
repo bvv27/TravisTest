b69405cafce6c5203e1c2b0a4c69f255923d7879
bvv27 <bvv.developer@gmail.com>
2015-10-11 21:11:01 +0300

    0003 nunit

---
+++ TravisTest/test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TravisTest
+{
+    [TestFixture, Description("Tests Account")]
+    public class Test
+    {
+
+        [Test, Description("Tests account")]
+        public void TestGet()
+        {
+            var class1 = new Class1();
+            Assert.IsNotNull(class1);
+        }
+    }
+}
